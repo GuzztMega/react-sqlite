d6a9223f87ae2b24b4662b466055c1587f91c923
guzzmega <guzzt.mega@gmail.com>
2024-08-17 21:17:08 -0300

    set initial page and configurations

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { Input } from '@/components/Input';
+import { useState } from 'react';
+import { View, Button } from 'react-native';
 
-export default function Index(){
+export default function Index() {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [products, setProducts] = useState([]);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 30}}>GuzzMega</Text>
+    <View style={{ flex: 1, justifyContent: 'center', padding: 30, gap: 16 }}>
+      <Input placeholder='Name' />
+      <Input placeholder='Quantity' />
+      <Button title='Save' />
     </View>
   )
 }
